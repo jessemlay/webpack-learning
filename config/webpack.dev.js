f639e862a1792887de2726485a31379ec75380de
Jesse <jesse.lay@rastracc.com>
2018-01-17 17:56:45 -0600

    init

---
+++ config/webpack.dev.js
@@ -0,0 +1,13 @@
+const path = require('path');
+module.exports = {
+    entry: {
+        main: ["./src/main.js"]
+    },
+    output: {
+        filename: "[name]-bundle.js",
+        path: path.resolve(__dirname, "../dist")
+    },
+    devServer: {
+        contentBase: "dist"
+    }
+}
